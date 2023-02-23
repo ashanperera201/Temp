4a914dcce5954ae37cefa2d1538fd0ce181b8004
Ashan Perera <AshanPerera.code@gmail.com>
2023-02-23 16:16:36 +0530

    sql email..

---
+++ imi/SRM2/SRM/Sql/Email_Template.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[EMAIL_TEMPLATES](
+  [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  [TemplateUniqueId] NVARCHAR(MAX) NOT NULL,
+  [EmailTemplateCode] NVARCHAR(MAX) NOT NULL,
+  [EmailTemplateName] NVARCHAR(150) NOT NULL,
+  [HtmlContent] NVARCHAR(MAX) NULL,
+  [FooterSignature] NVARCHAR(MAX) NULL,
+  [HeaderSignature] NVARCHAR(MAX) NULL,
+  [ThanksGivingSection] NVARCHAR(MAX) NULL,
+  [IsActive] BIT NOT NULL,
+  [CreatedBy] NVARCHAR(MAX) NOT NULL,
+  [CreatedOn] DATETIME NOT NULL,
+  [UpdatedBy]	NVARCHAR(MAX) NULL,
+  [UpdatedOn]	DATETIME NULL
+);
